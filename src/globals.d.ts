660bbcba9fa686142bb770864ac980a72bb6c4cf
levi <cgfeel@gmail.com>
2024-06-12 16:26:17 +0800

    first commit

---
+++ src/globals.d.ts
@@ -0,0 +1,7 @@
+declare var __webpack_public_path__: string;
+declare var __MICRO_APP_BASE_ROUTE__: string | undefined;
+
+declare interface Window {
+    __MICRO_APP_ENVIRONMENT__: boolean | undefined;
+    __MICRO_APP_PUBLIC_PATH__: string | undefined;
+}
---
+++ src/public-path.ts
@@ -0,0 +1,5 @@
+if (window.__MICRO_APP_ENVIRONMENT__ && window.__MICRO_APP_PUBLIC_PATH__) {
+  __webpack_public_path__ = window.__MICRO_APP_PUBLIC_PATH__;
+}
+
+export {};
